b0a624fd30b6fda139007969fbf9d326380fa4dc
Surya-17 <suryakartheekb@gmail.com>
2022-01-20 15:34:06 +0530

    file upload

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CoinScript : MonoBehaviour {
+     public int score = 0;
+    // Use this for initialization
+    void Start () {
+  
+    }
+  
+    // Update is called once per frame
+    void Update () {
+  
+    }
+    void OnTriggerEnter2D(Collider2D other){
+          Debug.Log ("Triggered");
+        Destroy (gameObject);
+        score++;
+        Debug.Log ("Score: " + score);
+    }
+
+}
+
+
